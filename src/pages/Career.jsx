40fdf45387c4114ba3bad0e94691468665629f13
Huovinen Teemu <teemu.huovinen@dagmar.fi>
2018-07-06 22:26:21 +0300

    Projects -> Career

---
+++ src/pages/Career.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import './Career.scss'
+
+const Career = () => (
+    <div className="Career">
+        <h2>
+            CAREER
+        </h2>
+
+        <p className="Career-Text">
+            More information coming at some point
+        </p>
+
+    </div>
+)
+
+export default Career
+
+
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import HeroBlock from './HeroBlock'
 import HeroImage from './HeroImage'
 import About from './About'
-import Projects from './Projects'
+import Career from './Career'
 
 const IndexPage = () => (
     <div>
         <HeroImage />
         <HeroBlock />
         <About />
-        <Projects />
+        <Career />
     </div>
 )
 
