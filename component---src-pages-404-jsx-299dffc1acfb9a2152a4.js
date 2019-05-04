3eb8c94ef211a30a39cae8edf12a356daccaae6c
Teemu Huovinen <teemu@swappie.com>
2019-05-04 10:27:39 +0300

    Updates

---
+++ component---src-pages-404-jsx-299dffc1acfb9a2152a4.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{138:function(e,t,n){"use strict";n.r(t);var a=n(0),s=n.n(a);n(152);t.default=function(){return s.a.createElement("div",{className:"NotFound"},s.a.createElement("h1",null,"NOT FOUND"),s.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}},152:function(e,t,n){}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-299dffc1acfb9a2152a4.js.map
